c382c2e93f653052345aa5f2f3993ebcb468ce11
Adam <adam@ventureinteractive.com>
2014-05-27 11:37:49 -0400

    Implementation of a deck of cards

---
+++ card.rb
@@ -0,0 +1,32 @@
+class Card
+  include Comparable
+
+  attr_reader :rank, :suit, :value
+
+  def initialize rank, suit, value
+    @rank  = rank
+    @suit  = suit
+    @value = value
+  end
+
+  def red?
+    ( [%w{Hearts Diamonds}].include? @suit ) ? true : false
+  end
+
+  def face_card?
+    ( [%w{J Q K A}].include? @rank ) ? true : false
+  end
+
+  def <=> card
+    self.value <=> card.value
+  end
+  
+  def to_s
+    "#{@rank} of #{@suit}"
+  end
+
+  def inspect
+    "#<#{@rank} of #{@suit} valued at #{value}>"
+  end
+
+end
---
+++ deck_of_cards.rb
@@ -0,0 +1,48 @@
+require './card.rb'
+
+class DeckOfCards
+
+	attr_reader :cards
+
+	RANKS = %w{2 3 4 5 6 7 8 9 10 J Q K A}
+	SUITS = %w{Clubs Diamonds Hearts Spades}
+
+	def initialize
+    @cards = []
+    SUITS.each do |suit|
+    	RANKS.each_with_index do |rank, i|
+    		@cards.push Card.new( rank, suit, i+1 )
+    	end
+    end
+  end
+
+  def deal number
+  	number.times.collect { @cards.shift }.compact!
+  end
+  
+  def draw_first
+    @cards.shift
+  end
+  
+  def draw_random
+  	@cards.delete_at( rand( @cards.length ) )
+  end
+
+  def order
+  	@cards.sort_by { |card| [SUITS.index(card.suit), RANKS.index(card.rank)] }
+  end
+
+  def search_for_card suit, rank
+  	@cards.select { |card| card.suit == suit && card.rank.to_s == rank.to_s }.first
+  end
+
+	def shuffle
+		@cards.shuffle!
+	end
+
+	def split
+		slices = @cards.each_slice( ( @cards.size / 2.0 ).round ).to_a
+		@cards = slices.insert(0, slices.delete_at(1)).flatten
+	end
+
+end
